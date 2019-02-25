063c4443a4e0dcf88660cc01d64b39e2a2a40049
b18030 <tushar22.tg.tg@gmail.com>
2019-02-25 14:28:54 +0530

    Sorting syntax for dictionary by value and how to use key parameter in dictionary

---
+++ sortsyntax.py
@@ -0,0 +1,13 @@
+
+
+
+sort() Parameters
+By default, sort() doesn't require any extra parameters. However, it has two optional parameters:
+
+reverse - If true, the sorted list is reversed (or sorted in Descending order)
+key - function that serves as a key for the sort comparison
+
+
+
+
+Sorting dictionary
